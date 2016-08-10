7ee79d15d0fce3b37d736cc7b180071b93b41783
EmersonLopes <emersoncold2@gmail.com>
2016-08-10 11:18:00 -0300

    ok

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var http = require('http');
 var app  = require('./config/express');
-var db   = require('./config/database') ('emerson:1903890711@ds042729.mlab.com:42729/sofisticada');
+var db   = require('./config/database')('emerson:1903890711@ds042729.mlab.com:42729/sofisticada');
 
 http.createServer(app).listen(2000, function () {
     console.log('Servidor rodando na porta: ' + this.address().port);
