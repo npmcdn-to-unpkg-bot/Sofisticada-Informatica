32b2e70870aacdb1b238e715198f592fc1a4fc6f
EmersonLopes <emersoncold2@gmail.com>
2016-08-10 10:31:27 -0300

    ok

--- config/express.js
+++ config/express.js
@@ -5,6 +5,11 @@ var bodyParser = require('body-parser');
 //var routes = require('../app/routes');
 //-------------------
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.use(express.static('./public'));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
